a6f728c37d7c965688ffe194337347caef7d0c6f
benjie13 <45374835+benjie13@users.noreply.github.com>
2018-11-27 07:53:32 +0800

    test commit file

---
+++ sample_lattao.py
@@ -0,0 +1,31 @@
+# Declaring and initializing variables in Phyton
+
+#Strings
+Ako = "mabait"
+rommel = "banal"
+jushwill = "gwapings"
+
+#Numerical values
+ilankamimagkakapatid = 2
+ilansila= 6
+ganokaliit = 3
+
+# Arithmetic
+addition = 2 + 5
+subtraction = 3 - 3
+
+print(Ako)
+print(rommel)
+print(jushwill)
+print(ilankamimagkakapatid)
+print(ilansila)
+print(ganokaliit)
+print(addition)
+print(subtraction)
+
+if addition > subtraction:
+    print("GG")
+elif addition < subtraction:
+    print("BB")
+else:
+    print("BYE")
